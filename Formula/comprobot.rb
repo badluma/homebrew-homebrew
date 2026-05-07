a32b9682c2c878d058aa778e6b6221c2b86d373f
badluma <commune-nice.5w@icloud.com>
2026-05-07 21:00:44 +0200

    feat: add comprobot formula
    Initial Homebrew tap with Comprobot v2.1.0 formula.

---
+++ Formula/comprobot.rb
@@ -0,0 +1,22 @@
+class Comprobot < Formula
+  include Language::Python::Virtualenv
+
+  desc "Self-hostable Discord bot built for maximum customization"
+  homepage "https://badluma.github.io/Comprobot-Docs"
+  url "https://github.com/badluma/Comprobot/archive/refs/tags/v2.1.0.tar.gz"
+  sha256 "PLACEHOLDER_RUN_sha256sum_ON_RELEASE_TARBALL"
+  license "MIT"
+  head "https://github.com/badluma/Comprobot.git", branch: "main"
+
+  depends_on "python@3.12"
+
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    venv.pip_install buildpath
+    bin.install_symlink libexec/"bin"/"comprobot"
+  end
+
+  test do
+    assert_match "usage", shell_output("#{bin}/comprobot --help 2>&1")
+  end
+end
